f980f03d0cb01cf7e0cd32ed629c32ed233b7909
Shelby Goudy <shelbygoudy@gmail.com>
2020-05-23 11:24:57 -0400

    Step 8 Complete
    Wrapped game variable in a function so it wouldn't fire on load

--- js/app.js
+++ js/app.js
@@ -2,10 +2,8 @@
  * Project 4 - OOP Game App
  * app.js */
 
-
- const game = new Game(); 
- game.startGame(); 
- console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+let game;
+document.getElementById('btn__reset').addEventListener('click', function(){game = new Game().startGame()});
 
  
 
